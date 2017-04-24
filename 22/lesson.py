6dff2d9d36dffed406a89797a5054f9db63f847f
YanHengGo <dang712@hotmail.com>
2017-04-25 05:09:40 +0900

    小甲鱼零基础入门学习python22 factorial

---
+++ 22/lesson.py
@@ -0,0 +1,17 @@
+def factorial(n):
+    if n==1:
+        return 1
+    return n*factorial(n-1)
+
+print(factorial(5))
+
+def fatorial1(n):
+    result=n
+    for i in range(1,n):
+        result *= i
+
+    return result
+
+num=int(input("please enter a num:"))
+result=factorial(num)
+print("%d 的阶乘是 %d" % (num,result))
