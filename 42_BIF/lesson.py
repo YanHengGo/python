69df8e8a37cbd093569f12c10c138748f63b2b75
YanHengGo <dang712@hotmail.com>
2017-05-10 05:33:00 +0900

    BIF 运算符的重定义

--- 42_BIF/lesson.py
+++ 42_BIF/lesson.py
@@ -23,3 +23,29 @@ a=New_int(3)
 b=New_int(5)
 print("a+b=",a+b)
 print("a-b=",a-b)
+
+print('-----------')
+#封装int 重写加法定义 另一种写法
+class New_int(int):
+    def __add__(self,other):
+        return int(self)+int(other)
+    def __sub__(self,other):
+        return int(self)-int(other)
+
+a=New_int(3)
+b=New_int(5)
+print("a+b=",a+b)
+print("a-b=",a-b)
+
+#封装int 重写加法定义 死循环
+class New_int(int):
+    def __add__(self,other):
+        return self+other
+    def __sub__(self,other):
+        return self-other
+
+
+print(type(int))
+a=3
+print(a>>2)
+print(a<<2)
